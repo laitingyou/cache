f8069d23d2eca4b23ccb7c8cf61cf7f1ae31b154
laitingyou <laitingyou@outlook.com>
2020-01-13 16:23:54 +0800

    fix: bug

---
+++ test/index.js
@@ -0,0 +1,21 @@
+import {cache} from 'ltycache'
+const cache = new cache()
+// 异步
+cache.setItem('name', 'laitingyou', 2000).then(res=>{
+  console.log(res)
+}).catch(e=>{})
+cache.getItem('name').then(res=>{
+  console.log(res)
+}).catch(e=>{})
+//同步
+let res1 = cache.syncSetItem('name', 'laitingyou', 2000)
+console.log(res1)
+
+let res2 = cache.syncGetItem('name')
+console.log(res2)
+
+
+
+
+
+
